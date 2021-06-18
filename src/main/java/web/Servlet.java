b5927df6c1d322b58668fc5be1716302a78ec838
ednerPE <ednerpesquera@hotmail.com>
2021-06-18 17:21:39 -0600

    Version 1.1
    --Formulario con servlet

---
+++ src/main/java/web/Servlet.java
@@ -0,0 +1,14 @@
+package web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author EdnerPA
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+
+
+}
