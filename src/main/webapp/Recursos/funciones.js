b2603a3ab130ac64fbd47a64d837ab56dce6db79
ednerPE <ednerpesquera@hotmail.com>
2021-07-06 10:46:52 -0600

    Version 1.3
    --Formulario con servlet terminado

--- src/main/webapp/Recursos/funciones.js
+++ src/main/webapp/Recursos/funciones.js
@@ -1,7 +1,61 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+function validarForm(forma){
+     var usuario = forma.usuario;
+     if(usuario.value == "" || usuario.value == "Escriba un usuario"){
+         alert("Debe proporcionar un nombre de usuario");
+         usuario.focus();
+         usuario.select();
+         return false;
+         
+     }
+    
+    var password = forma.password
+    if(password.value == "" || password.value.length < 3){
+        alert("Debe proporcionar un password al menos de 3 caracteres");
+        password.focus();
+        password.select();
+        return false;
+    }
+    
+    var tecnologia = forma.tecnologia;
+    var checkSeleccionado = false;
+    
+    
+    for(var i = 0; i < tecnologia.length; i++){
+        if(tecnologia[i].checked){
+            checkSeleccionado = true;
+        }
+        
+    }
+    
+    if(!checkSeleccionado){
+        alert("Debe seleccionar una tecnologia por lo menos");
+        return false;
+    }
+    
+    var genero = forma.genero;
+    var radioSelecionado = false;
+    
+    for(var i =0; i < genero.length; i++){
+        if(genero[i].checked){
+            radioSelecionado = true;
+        }
+    }
+    
+    
+    if(!radioSelecionado){
+        alert("debe seleccionar un genero");
+        return false;
+    }
+    
+    var ocupacion = form.ocupacion;
+    if(ocupacion.vaue == ""){
+        alert("Debe seleccionar una ocupacion");
+        return false;
+    }
+    
+   //formulario es valida 
+   alert("Formulario valido enviado datos al servidor");
+   return true;
+   
+}
 
